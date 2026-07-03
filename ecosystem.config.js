794d5396403b3f241d17395d301775cac14f4915
root <root@localhost.localdomain>
2026-07-04 00:29:09 +0800

    chore: remove copyright fingerprint from all source files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,3 @@
-/**
- * ========================================
- * 盐阜配送 - Yanfu Delivery
- * ========================================
- * © 中哥  All Rights Reserved. 商用需联系本人授权
- * FP_UUID_31adb5871aea40b8b0c288773f094ab2|FP_AUTHOR_中哥_SN_20260531|FP_HASH_20260531B9F3|FP_ORIGIN_2026_AUTHOR_中哥
- * ========================================
- * 严禁未经授权转载、商用，商用需联系作者授权
- * 遵循开源协议，仅限项目内部使用，商用需联系本人授权
- * ========================================
- */
-
 module.exports = {
   apps: [{
     name: 'yanfu-backend',
@@ -37,7 +25,4 @@ module.exports = {
   }]
 };
 
----
 
-© 中哥  All Rights Reserved. 商用需联系本人授权
-FP_UUID_31adb5871aea40b8b0c288773f094ab2|FP_AUTHOR_中哥_SN_20260531|FP_HASH_20260531B9F3|FP_ORIGIN_2026_AUTHOR_中哥
